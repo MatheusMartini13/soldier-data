b92d198e35bfbc8ddaf82edeb9deeba29ad52a0e
MmatheusmMartini13 <matheusmartini13@gmail.com>
2022-09-29 20:37:29 -0300

    simple exercise

---
+++ data.js
@@ -0,0 +1,11 @@
+alert ("Answer all the questions.");
+let soldierName = prompt ("What is your first name?");
+let soldierSurname = prompt ("What is your last name?");
+let fieldOfStudy = prompt ("What is your field of Study?");
+let birthDate = prompt ("What`s the year of your birth");
+
+let age = 2022 - birthDate
+
+console.log("Hello " + soldierName + " " + soldierSurname);
+console.log("You work as " + fieldOfStudy);
+console.log("Your age is " + age);
